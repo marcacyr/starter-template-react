61b056b821eefba7eb75940a4375ae2145ad6717
Jimmy <jgs2017@gmail.com>
2017-07-15 15:32:14 -0500

    add redux and react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import { Router, browserHistory } from 'react-router';
+import { getRoutes } from './routes';
+import { syncHistoryWithStore } from 'react-router-redux';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStoreWithMiddleware(
+  reducers,
+  window.devToolsExtension ? window.devToolsExtension() : undefined
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      { getRoutes(store) }
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer as routing } from 'react-router-redux';
+
+const appReducer = combineReducers({
+  routing,
+});
+
+const rootReducer = (state, action) => appReducer(state, action);
+
+export default rootReducer;
